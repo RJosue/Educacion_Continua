e56554ee78948c2ab1e2761968d870098f13b416
RJosue <raul56480@gmail.com>
2019-12-10 22:55:33 -0500

    error

--- php/autentificacion.php
+++ php/autentificacion.php
@@ -1,11 +1,11 @@
 <?php
 //Iniciar la sesión 
-session_start(); 
+session_start();
 // verifica la sesión
 
-$user =$_POST['user'];
+$user = $_POST['user'];
 //$pass =md5($_POST['password']);
-$pass = ($_POST['password']);
+$pass = md5($_POST['password']);
 include 'conexion.php';
 //select u.id, u.nombre, u.apellido, ur.id_rol, r.rol from usuarios u inner join usuarios_rol ur on u.id = id_usuario inner join rol r on r.id = ur.id_rol where u.correo = ? and u.password = ? 
 
@@ -20,27 +20,26 @@ include 'conexion.php';
 // 				}
 
 $stmt = $conexion->prepare("SELECT u.id, u.nombre, u.apellido, u.cedula, u.correo, u.fecha_nacimiento, ur.id_rol, r.rol from usuarios u inner join usuarios_rol ur on u.id = id_usuario inner join rol r on r.id = ur.id_rol where u.correo = ? and u.password = ? LIMIT 1");
-$stmt->execute([$user, $pass]); 
+$stmt->execute([$user, $pass]);
 $data = $stmt->fetchAll();
 $count = $stmt->rowCount();
 echo $count;
-if($count){
-        foreach ($data as $row) {
-        echo $row['nombre'].$row['id_rol'];
+if ($count) {
+    foreach ($data as $row) {
+        echo $row['nombre'] . $row['id_rol'];
         $_SESSION['nombre'] = $row['nombre'];
         $_SESSION['apellido'] = $row['apellido'];
-        $_SESSION['cedula']= $row['cedula'];
+        $_SESSION['cedula'] = $row['cedula'];
         $_SESSION['correo'] = $row['correo'];
         $_SESSION['fecha_nacimiento'] = $row['fecha_nacimiento'];
         $_SESSION['id'] = $row['id'];
         $_SESSION['id_rol'] = $row['id_rol'];
         $_SESSION['rol'] = $row['rol'];
         header('Location: ../index.php');
-        }
     }
-else{
+} else {
     echo "<script>alert('Usuario o Contraseña incorrecto.'); window.location='login.php';</script>";
-    }
+}
 
 
 
