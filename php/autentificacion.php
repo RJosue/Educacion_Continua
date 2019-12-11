7618cf9faf9090d8f731e5aba9bbbde670fdf79f
NetoCh <ernesto11998@yahoo.com>
2019-12-11 00:25:22 -0500

    modificacion de autentificacion

--- php/autentificacion.php
+++ php/autentificacion.php
@@ -9,17 +9,7 @@ $pass =$_POST['password'];
 include 'conexion.php';
 //select u.id, u.nombre, u.apellido, ur.id_rol, r.rol from usuarios u inner join usuarios_rol ur on u.id = id_usuario inner join rol r on r.id = ur.id_rol where u.correo = ? and u.password = ? 
 
-// echo $user.$pass;
-// $stmt = $conexion->prepare("select * from usuarios where correo = ? and password ?");
-// 				$stmt->execute([$user, $pass]); 
-//                 $data = $stmt->fetchAll();
-//                 echo $user.$pass;
-//                 foreach ($data as $row) {
-//                     echo $user.$pass. " asdasd";
-//                     echo $row['nombre']." ".$row['apellido']." ".$row['correo'];
-// 				}
-
-$stmt = $conexion->prepare("SELECT u.id, u.nombre, u.apellido, ur.id_rol, r.rol from usuarios u inner join usuarios_rol ur on u.id = id_usuario inner join rol r on r.id = ur.id_rol where u.correo = ? and u.password = ? LIMIT 1");
+$stmt = $conexion->prepare("SELECT u.id, u.nombre, u.apellido, ur.id_rol, r.rol from usuarios u inner join usuarios_rol ur on u.id = iur.d_usuario inner join rol r on r.id = ur.id_rol where u.correo = ? and u.password = ? LIMIT 1");
 $stmt->execute([$user, $pass]); 
 $data = $stmt->fetchAll();
 $count = $stmt->rowCount();
